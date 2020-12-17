7361864f3ad79079d4963cba670e793b36c62315
RemingtonSteele <59892481+R-Steel3@users.noreply.github.com>
2020-12-17 10:15:16 -0500

    update:(

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var d = new Date();
 document.getElementById("currentDay").innerHTML = d;
 
-.$("button").click(function (e) { 
-    e.preventDefault();
-    
+var newEvent = {
+    types: ['Mindful', 'Productive', 'Fun', 'Important'],
+    hours: ['7 AM', '8 AM', '9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM'],
+    days:  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    text: (""),
+}
+
+document.getElementsByClassName("time-block").addEventListner("click", function () {
+    document.getElementById("eventAdd").innerHTML = ("");
 });
